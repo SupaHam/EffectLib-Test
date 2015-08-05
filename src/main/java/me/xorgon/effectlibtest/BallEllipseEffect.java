35d561fcb90cabb906dab9438fcad343dd7ac70a
Elijah Andrews <norman.reggox@gmail.com>
2015-08-05 17:02:27 +0100

    Whooops.

--- src/main/java/me/xorgon/effectlibtest/BallEllipseEffect.java
+++ src/main/java/me/xorgon/effectlibtest/BallEllipseEffect.java
@@ -1,7 +1,55 @@
 package me.xorgon.effectlibtest;
 
+import com.supaham.effectlibtest.Serializers;
+import de.slikey.effectlib.Effect;
+import de.slikey.effectlib.EffectManager;
+import de.slikey.effectlib.util.ParticleEffect;
+import org.bukkit.Location;
+import org.bukkit.util.Vector;
+
+import java.util.Map;
+
 /**
- * Created by Elijah on 05/08/2015.
+ * An effect that flies around the player.
  */
-public class BallEllipseEffect {
+public class BallEllipseEffect extends Effect {
+
+    Vector r; //Location relative to player
+    Vector v; //Velocity
+    Vector a; //Acceleration
+
+    public BallEllipseEffect(EffectManager effectManager) {
+        super(effectManager);
+    }
+
+    @Override
+    public void onRun() {
+        Location location = getLocation();
+        ParticleEffect particle = ParticleEffect.REDSTONE;
+        if (r == null) {
+            r = new Vector(1, 0, 0);
+        }
+        if (v == null) {
+            v = new Vector(0, 0, 0.05);
+        }
+        if (a == null) {
+            a = r.multiply(-0.05);
+        }
+
+        display(particle, location.clone().add(r));
+        updateVectors();
+    }
+
+    private void updateVectors() {
+        v = v.add(a);
+        r = r.add(v);
+    }
+
+    public static final class BallEllipse implements Serializers.Serializer<BallEllipseEffect> {
+
+        @Override
+        public void deserialize(BallEllipseEffect effect, Map<String, String> map) {
+        }
+
+    }
 }
